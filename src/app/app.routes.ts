f893fea820557e6b21c6bdcb15254d83cd2aae5d
RobsonFe <robson_20ferreira@hotmail.com>
2024-05-23 21:13:03 -0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LocacaoComponent } from './components/locacao/locacao.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: " ", component:LoginComponent},
+  {path: "/login", component:LoginComponent},
+  {path: "/home", component:HomeComponent},
+  {path: "/locacao", component:LocacaoComponent}
+];
