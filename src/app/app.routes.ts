bce903128069dce846791a9150b21d1e85602a00
RobsonFe <robson_20ferreira@hotmail.com>
2024-05-28 16:50:56 -0300

    fix:home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LocacaoComponent } from './components/locacao/locacao.component';
 import { ConsultarComponent } from './components/consultar/consultar.component';
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: ' ', component: HomeComponent },
   { path: 'cadastrar', component: LocacaoComponent },
   { path: 'consultar', component: ConsultarComponent },
   { path: 'cadastrar/:id', component: LocacaoComponent },
